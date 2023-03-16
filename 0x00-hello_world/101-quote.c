b56a9f788e79c237a3eac5957ca4011f8f14f2cb
MainaFiona <mainawanguifiona@gmail.com>
2023-03-16 11:16:20 -0700

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print program without puts or printf function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful" "- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
