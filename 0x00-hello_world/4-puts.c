00b7a0fe145a943995fa702e274c469fcb3191d9
MainaFiona <mainawanguifiona@gmail.com>
2023-03-16 09:04:45 -0700

    edit  Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-/*
+/**
  * main - C program that prints using puts function
  *
  * Return: 0
  */
- 
-int main(void){
-
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl e");
+	return (0);
 }
 
