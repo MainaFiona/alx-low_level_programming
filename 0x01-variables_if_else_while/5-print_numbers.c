f4e5ddc0de5b70780809856b47161b7ababe7d84
MainaFiona <mainawanguifiona@gmail.com>
2023-03-17 10:14:22 -0700

    single digit num from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
