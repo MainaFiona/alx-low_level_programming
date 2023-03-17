edbb029b4a9ade80c2cc2f22aca8383741124b1f
MainaFiona <mainawanguifiona@gmail.com>
2023-03-17 11:30:24 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
- * main - program that prints lowercase in reverse
+ * main - main function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int i;
 	char c;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
