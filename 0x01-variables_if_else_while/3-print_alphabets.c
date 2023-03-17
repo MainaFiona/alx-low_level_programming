bb958b29a1fda0a9b35322600e9f24b6038e40e5
MainaFiona <mainawanguifiona@gmail.com>
2023-03-17 09:23:18 -0700

    lowercase to uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that prints lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+        {
+                putchar(c);
+        }
+	putchar('\n');
+	return (0);
+}
