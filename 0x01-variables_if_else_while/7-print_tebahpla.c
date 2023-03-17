6bb76c656e1aeb4f7625810b84c92bff1c0b0fdc
MainaFiona <mainawanguifiona@gmail.com>
2023-03-17 10:46:50 -0700

    alphabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
