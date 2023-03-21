3981b504a3f20fb49f46710ae7ece0dd8269a47b
MainaFiona <mainawanguifiona@gmail.com>
2023-03-21 20:31:40 +0000

    use while loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,32 +10,25 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n >= 0 && n <= 98)
+	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		while (n <= 98)
 		{
-			printf("%d", i);
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n = n + 1;
 		}
-		printf("\n");
-	}
-	else if (n < 0)
+	} else if (n > 98)
 	{
-		for (i = n; i <= 98; i++)
+		while (n >= 98)
 		{
-			printf("%d, ", i);
+			if (n == 98)
+				printf("%d\n", n);
+			else
+				printf("%d, ", n);
+			n = n - 1;
 		}
-		printf("\n");
 	}
-	else if (n > 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			 printf("%d, ", i);
-		}
-		printf("\n");
-	}
-	else if (n == 98)
-		printf("%d, ", n);
 }
