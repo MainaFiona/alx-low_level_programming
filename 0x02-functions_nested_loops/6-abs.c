cffabac05a79d0e44e8f96094f0aed01cdd76b15
MainaFiona <mainawanguifiona@gmail.com>
2023-03-21 17:54:35 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an interger
+ *
+ * @n: int in check
+ *
+ * Return: absolute value of int
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return(-n)
+}
