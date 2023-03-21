778287a6a262872c0440018a054309134c204e48
MainaFiona <mainawanguifiona@gmail.com>
2023-03-21 19:44:30 +0000

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum of both a and b
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
