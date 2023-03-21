c973700397352e38d78723e41f7022b218fe626a
MainaFiona <mainawanguifiona@gmail.com>
2023-03-21 07:23:49 -0700

    edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
- * main - check the code
- * _putchar: print putchar characters
- * Return: Always 0
- */
+* main - prints putchar
+* Return: always 0
+*/
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
