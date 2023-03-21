74e2a6685a5dfb707fc101cf04e81b0750a6445a
MainaFiona <mainawanguifiona@gmail.com>
2023-03-21 17:28:25 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: parameter for argument
+ *
+ * Return: 1 if +ve, 0 if zero, -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	return (-1);
+}
