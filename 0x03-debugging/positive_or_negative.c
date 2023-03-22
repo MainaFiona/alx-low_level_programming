7c7320f2fb3c4fac9742ff8014f0d20889e85541
MainaFiona <mainawanguifiona@gmail.com>
2023-03-22 11:22:58 +0000

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * positive_or_negative - positive or negrative usinf if statement
+ *
+ * @i: number to check
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
+	
+}
