c32967366938dd29b2e6606bf3d6b5c5c5430c34
MainaFiona <mainawanguifiona@gmail.com>
2023-03-23 10:38:30 +0000

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - mu;tiplies two integers
+ * @a: first integer
+ * @b: second interger
+ * Return: 1 if true otherwise 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
