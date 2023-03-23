37b7cf46610062aafd01df5c9b20a4726d59537a
MainaFiona <mainawanguifiona@gmail.com>
2023-03-23 11:20:37 +0000

    remove whitespace

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_numbers - prints number from 0 t0 9
- * 
+ *
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i ='0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
