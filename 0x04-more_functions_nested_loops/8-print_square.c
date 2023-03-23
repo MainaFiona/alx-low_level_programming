b912cf424d83724edc461e2cc109673782b571c5
MainaFiona <mainawanguifiona@gmail.com>
2023-03-23 20:31:06 +0000

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - function that draws a square line
+ *
+ * @size: set square size
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+       	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
