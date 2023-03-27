86b292baf6c0c96a3128e346ce18cdc97c3a9cd4
MainaFiona <mainawanguifiona@gmail.com>
2023-03-27 15:16:27 +0000

    length of dtring

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length os a string
+ *
+ * @s: character to get string
+ * Return: return length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
