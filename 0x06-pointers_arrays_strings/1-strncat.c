da27c3d5e737d53252b5f5be2326de6090b9aad6
MainaFiona <mainawanguifiona@gmail.com>
2023-03-29 09:24:58 +0000

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * * _strcat - main function to concatenate two strings
+ * * _strncat - main function to concatenate two strings
  *
  * @dest: destination to copy
  * @src: source
  * @n: lines to append
  *
- * Return: destination
+ * Return: value of char
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++);
 
-	for (j = 0; src[j] != '\0'; j++, i++)
+	for (j = 0; j < n &&  src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}
