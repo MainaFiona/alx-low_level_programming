6c527508078d53f121f7d0d96a5c932023bd7c20
MainaFiona <mainawanguifiona@gmail.com>
2023-04-05 12:55:19 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int helper(int, int);
 /**
  * _sqrt_recursion - eturns the natural square root of a number
  *
@@ -8,5 +9,29 @@
  */
 int _sqrt_recursion(int n)
 {
+	return (helper(n, 1));
+}
 
+/**
+ * helper - checks square root of a number
+ * @i:helper
+ * @n: integer to test
+ * Return: return integer
+ */
+int helper(int n, int i)
+{
+	int sqr = i * i;
+
+	if (sqr > n)
+	{
+		return (-1);
+	}
+	if (sqr == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (helper(n, i + 1));
+	}
 }
