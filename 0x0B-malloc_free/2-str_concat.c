70900467962d437a5b5e996d048110264c803b11
MainaFiona <mainawanguifiona@gmail.com>
2023-04-11 14:41:05 +0000

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,32 +11,21 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int i;
-	int j;
+	int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	i = 0;
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
 	j = 0;
 	while (s2[j] != '\0')
-	{
 		j++;
-	}
 	s = malloc(sizeof(char) * (i + j + 1));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	i = 0;
 	while (s1[i] != '\0')
 	{
