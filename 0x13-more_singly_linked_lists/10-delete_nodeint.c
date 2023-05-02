77deec270d0806e76cc3785009363cab8c281302
MainaFiona <mainawanguifiona@gmail.com>
2023-05-02 16:56:34 +0000

    delete list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = temp->next;
 		free(*head);
 		*head = temp;
-		return (-1);
+		return (1);
 	}
 	else
 	{
@@ -41,6 +41,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		delete = temp->next;
 		temp->next = temp->next->next;
 		free(delete);
-		return (-1);
+		return (1);
 	}
 }
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *reverse_listint - reverses a linked list
+ * @head: start of node
+ * Return: the address of the new node
+ */
