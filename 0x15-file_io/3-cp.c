b7d759d1f46267611f6d83caac8623033a9dbed7
MainaFiona <mainawanguifiona@gmail.com>
2023-05-08 18:33:21 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,16 +18,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (argv[2] == NULL)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
-		exit(99);
-        }
 	f1 = open(argv[1], O_RONLY);
 	if (f1 == -1)
 	{
@@ -35,30 +25,34 @@ int main(int argc, char *argv[])
                 exit(98);
 	}
 	f2 = open(argv[2], O_TRUNC | 0_CREATE | O_WRONLY, 0664);
-	while ((f3 == read(f1, buffer, 1024)) > 0) 
+	if (f2 == -1)
 	{
-		if (f3 == -1 || (write(f1, buffer, f3) != f3))
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
-	if (f3 == -1)
+	do
+	{
+		count1 = read(f1, buffer, 1024); 
+		if (count1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (count2 == -1)
+                {
+                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+                        exit(99);
+                }
+	} while (count1 == 1024)
+	if (count1(f1) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
                 exit(98);
 	}
-	count1 = close(f1);
-	if (count1 == -1)
+	if (count2(f2) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
                 exit(100);
 	}
-	count2 = close(f2);
-        if (count2 == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
-                exit(100);
-        }
 	return (0);
 }
